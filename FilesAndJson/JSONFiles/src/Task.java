0974b8e75164900463692cc8b41e3426fe597c53
GauravKumarCodes <gkumar196.gk@gmail.com>
2021-06-24 14:52:04 -0500

    first version

--- FilesAndJson/JSONFiles/src/Task.java
+++ FilesAndJson/JSONFiles/src/Task.java
@@ -12,7 +12,7 @@ public class Task {
         System.out.println(db.getAvgReviews("Casa Enrique"));
     }
     public String readFile(String filename) {
-        String contents = ""; //delete this later part 2
+        String contents = ""; 
 
         try {
             InputStream in = new BufferedInputStream(new FileInputStream(filename));
